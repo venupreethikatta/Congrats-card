7806617b777ad8d5efa5cf2c8b87daa238e44cc6
venupreethikatta <kattavenupreethi@gmail.com>
2023-06-19 12:05:00 +0000

    Congrats Card

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  // Write your code here.
+  <div className="div-container">
+    <h1 className="heading-class">Congratulations</h1>
+    <div className="inside-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="heading-class2">Kiran V</h1>
+      <p className="para-1">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
